0d55681e22107c6dda3368dd70586cac0e43f4d1
jhampl <joshuahampl@gmail.com>
2018-08-16 16:49:44 +0200

    initial commit

--- JPEGCompression/Compression.py
+++ JPEGCompression/Compression.py
@@ -4,31 +4,39 @@ import numpy as np
 from PIL import Image
 import math
 from collections import Counter
+import seaborn as davi
+from shutil import copyfile
+import os
 
 np.set_printoptions(threshold=np.nan)
+
 
 class JPEG:
 
     def __init__(self, img):
         self.img = np.array(Image.open(img))
+        self.ordner = 'Ergebnisse'
 
 
-    def hinTransformation(self,matrix):
-        ycbcr = np.array([[.299, .587, .114], [-.169, -.331, .5], [.5, -.419, -.081]])
+    def hinTransformation(self, matrix):
+        ycbcr = np.array(
+            [[.299, .587, .114], [-.169, -.331, .5], [.5, -.419, -.081]])
         for a in range(matrix.shape[0]):
             for b in range(matrix.shape[1]):
-                np.matmul(ycbcr,np.array([matrix[a][b][0],matrix[a][b][1],matrix[a][b][2]]), matrix[a][b])
+                np.matmul(ycbcr, np.array(
+                    [matrix[a][b][0], matrix[a][b][1], matrix[a][b][2]]), matrix[a][b])
         matrix[:, :, [1, 2]] += 128
         return np.round(matrix)
 
 
-    def rueckTransformation(self,matrix):
+    def rueckTransformation(self, matrix):
         ycbcr = np.array([[1, 0, 1.402], [1, -.34414, -.71414], [1, 1.722, 0]])
         matrix[:, :, [1, 2]] -= 128
 
         for a in range(matrix.shape[0]):
             for b in range(matrix.shape[1]):
-                np.matmul(ycbcr,np.array([matrix[a][b][0],matrix[a][b][1],matrix[a][b][2]]), matrix[a][b])
+                np.matmul(ycbcr, np.array(
+                    [matrix[a][b][0], matrix[a][b][1], matrix[a][b][2]]), matrix[a][b])
 
         np.putmask(matrix, matrix > 255, 255)
         np.putmask(matrix, matrix < 0, 0)
@@ -36,7 +44,7 @@ class JPEG:
         return matrix
 
 
-    def showState(self,matrix):
+    def showState(self, matrix):
         im = Image.fromarray(np.uint8(matrix))
         Image._show(im)
 
@@ -54,7 +62,7 @@ class JPEG:
         else:
             cropped_height = image.shape[1]
 
-        cropped_image = np.zeros((cropped_width, cropped_height,3))
+        cropped_image = np.zeros((cropped_width, cropped_height, 3))
 
         for rownum in range(len(cropped_image)):
             for colnum in range(len(cropped_image[rownum])):
@@ -85,16 +93,17 @@ class JPEG:
 
         for a in range(0, matrix.shape[0], 8):
             for b in range(0, matrix.shape[1], 8):
-                for z in range(0,3):
+                for z in range(0, 3):
                     for u in range(0, 8):
                         for v in range(0, 8):
 
                             help = 0
 
-                            for x in range(0,8):
-                                for y in range(0,8):
+                            for x in range(0, 8):
+                                for y in range(0, 8):
                                     help += matrix[a+x][b+y][z] * math.cos(((2 * x + 1) * u * math.pi) / 16) * \
-                                                                    math.cos(((2 * y + 1) * v * math.pi) / 16)
+                                        math.cos(
+                                            ((2 * y + 1) * v * math.pi) / 16)
 
                             if u == 0:
                                 cu = 1 / math.sqrt(2)
@@ -107,9 +116,9 @@ class JPEG:
 
                             help = help * 1/4 * cu * cv
 
-                           # matrix[a+u][b+v][z] = help
+                            # matrix[a+u][b+v][z] = help
 
-                            #print(help)
+                            # print(help)
                             if z == 0:
                                 helpMat[u][v] = int(round(help/QY[u][v]))
                             else:
@@ -142,13 +151,15 @@ class JPEG:
 
         for a in range(0, matrix.shape[0], 8):
             for b in range(0, matrix.shape[1], 8):
-                for z in range(0,3):
+                for z in range(0, 3):
                     for x in range(0, 8):
                         for y in range(0, 8):
                             if z == 0:
-                                matrix[a+x][b+y][z] = matrix[a+x][b+y][z]*QY[x][y]
+                                matrix[a+x][b+y][z] = matrix[a +
+                                                             x][b+y][z]*QY[x][y]
                             else:
-                                matrix[a+x][b+y][z] = matrix[a+x][b+y][z]*QC[x][y]
+                                matrix[a+x][b+y][z] = matrix[a +
+                                                             x][b+y][z]*QC[x][y]
         return matrix
 
 
@@ -157,14 +168,14 @@ class JPEG:
 
         for a in range(0, matrix.shape[0], 8):
             for b in range(0, matrix.shape[1], 8):
-                for z in range(0,3):
+                for z in range(0, 3):
                     for x in range(0, 8):
                         for y in range(0, 8):
 
                             help = 0
 
-                            for u in range(0,8):
-                                for v in range(0,8):
+                            for u in range(0, 8):
+                                for v in range(0, 8):
                                     if u == 0:
                                         cu = 1 / math.sqrt(2)
                                     else:
@@ -174,7 +185,8 @@ class JPEG:
                                     else:
                                         cv = 1
 
-                                    help += cu * cv * matrix[a+u][b+v][z] * math.cos(((2 * x + 1) * u * math.pi) / 16) * math.cos(((2 * y + 1) * v * math.pi) / 16)
+                                    help += cu * cv * matrix[a+u][b+v][z] * math.cos(
+                                        ((2 * x + 1) * u * math.pi) / 16) * math.cos(((2 * y + 1) * v * math.pi) / 16)
 
                             helpMat[x][y] = help * 1/4
 
@@ -196,7 +208,6 @@ class JPEG:
             for y in range(matrix.shape[1]):
                 if len(matrix.shape) == 3:
                     for z in range(matrix.shape[2]):
-                        counts[matrix[x][y][z]] += 1
                         i += 1
                 else:
                     counts[matrix[x][y]] += 1
@@ -235,22 +246,71 @@ class JPEG:
         return (nvm.entropie(matrix) - nvm.entscheidungsgehalt(matrix))
 
 
+    def drucke_histogramm(label, array):
+
+        plot = davi.distplot(array)
+        fig = plot.get_figure()
+        fig.savefig(pfad(label + '.png'))
+
+
+    def drucke_bild(label, array):
+
+        img = Image.fromarray(array, 'RGB')
+        img.save(pfad(label + '.png'))
+
+
+    def pfad(self, datei):
+
+        return os.path.abspath(self.ordner + datei)
+
+
 if __name__ == '__main__':
 
-    nvm = JPEG('/Users/Paul/Desktop/Master/2. Semester Master/Datenkompression/4.2.04.png')
+    # Initialisierung
+    src = os.path.abspath('../Testbilder/Lena/4.2.04.png')
+    nvm = JPEG(src)
 
+   # RGB Bild in Ergebnisse speichern
+    if not os.path.exists(nvm.ordner):
+        os.makedirs(nvm.ordner)
+    copyfile(src, nvm.pfad('rgb.png'))
+
+    # RGB Histogramme erstellen
+    nvm.drucke_histogramm('R', nvm.image[:, :, 0])
+    nvm.drucke_histogramm('G', nvm.image[:, :, 1])
+    nvm.drucke_histogramm('B', nvm.image[:, :, 2])
+
+    # Farbraumtransformation
     result = nvm.crop()
     result = nvm.hinTransformation(result)
 
-    # [R][G][B] - [Y][Cb][Cr]
+    # YCbCr Histogramme erstellen
+    nvm.drucke_histogramm('Y', result[:, :, 0])
+    nvm.drucke_histogramm('Cb', result[:, :, 1])
+    nvm.drucke_histogramm('Cr', result[:, :, 2])
 
-    print nvm.entropie(result[:,:,0])
-    print nvm.entscheidungsgehalt(result[:,:,0])
-    print nvm.quellenredundanz(result[:,:,0])
+    # Maße errechnen und schreiben
+    # RGB Maße
+    line = 'Komponente\tEntropie\tEntscheidungsgehalt\tQuellenredundanz'
+    with open(nvm.pfad('RGB_daten.txt')) as daten:
+        daten.write(line)
+
+    komp = 'Gesamt', 'Y', 'Cb', 'Cr'
+    for i in [':', 0, 1, 2]:
+        line += komp[i]
+        line += nvm.entropie(result[:, :, i])
+        line += nvm.entscheidungsgehalt(result[:, :, i])
+        line += nvm.quellenredunanz(result[:, :, i])
+
+        with open(nvm.pfad('RGB_daten.txt')) as daten:
+            daten.write(line)
+
+    # print nvm.entropie(result[:,:,0])
+    # print nvm.entscheidungsgehalt(result[:,:,0])
+    # print nvm.quellenredundanz(result[:,:,0])
 
     result = nvm.dct(result)
 
-    print nvm.entropie(result)
-    print nvm.entscheidungsgehalt(result)
-    print nvm.quellenredundanz(result)
-
+    # print nvm.entropie(result)
+    # print nvm.entscheidungsgehalt(result)
+    # print nvm.quellenredundanz(result)
